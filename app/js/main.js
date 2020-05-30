735173113fdac54afbae59bb060bc9b160d377e0
Tatyana Shevchenko <dafna2011@gmail.com>
2020-05-30 18:57:22 +0300

    aside category page

--- app/js/main.js
+++ app/js/main.js
@@ -1,35 +1,43 @@
-$(function(){
+$(function() {
     $('.featured-product-slider').slick({
 
-      });
-
-      $('.followers-slider').slick({
+    });
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 400,
+        from: 30,
+        to: 300,
+        grid: false,
+        prefix: "$"
+    });
+    $('.followers-slider').slick({
         slidesToShow: 3,
         slidesToScroll: 1
-    });  
+    });
     var mixer = mixitup('.new-items__box');
-    $(function () {
- 
+    $(function() {
+
         $(".rating").rateYo({
-          "rating": 4.6,
-          "starWidth": "15px",
-          "normalFill": "transparent",
-          "ratedFill": "#ffc000",
-          "readOnly": true,
-       
+            "rating": 4.6,
+            "starWidth": "15px",
+            "normalFill": "transparent",
+            "ratedFill": "#ffc000",
+            "readOnly": true,
+
         });
 
-        
+
         $(".rating-mini").rateYo({
             "rating": 4.6,
             "starWidth": "12px",
             "normalFill": "#eff1f5",
             "ratedFill": "#ffc000",
             "readOnly": true,
-         
-          });
-       
-      });
+
+        });
+
+    });
 
     $('.select').each(function() {
         const _this = $(this),
@@ -37,52 +45,53 @@ $(function(){
             selectOptionLength = selectOption.length,
             selectedOption = selectOption.filter(':selected'),
             duration = 450; // длительность анимации 
-    
+
         _this.hide();
         _this.wrap('<div class="select"></div>');
         $('<div>', {
             class: 'new-select',
             text: _this.children('option:disabled').text()
         }).insertAfter(_this);
-    
+
         const selectHead = _this.next('.new-select');
         $('<div>', {
             class: 'new-select__list'
         }).insertAfter(selectHead);
-    
+
         const selectList = selectHead.next('.new-select__list');
         for (let i = 1; i < selectOptionLength; i++) {
             $('<div>', {
-                class: 'new-select__item',
-                html: $('<span>', {
-                    text: selectOption.eq(i).text()
+                    class: 'new-select__item',
+                    html: $('<span>', {
+                        text: selectOption.eq(i).text()
+                    })
                 })
-            })
-            .attr('data-value', selectOption.eq(i).val())
-            .appendTo(selectList);
+                .attr('data-value', selectOption.eq(i).val())
+                .appendTo(selectList);
         }
-    
+
         const selectItem = selectList.find('.new-select__item');
         selectList.slideUp(0);
         selectHead.on('click', function() {
-            if ( !$(this).hasClass('on') ) {
+            if (!$(this).hasClass('on')) {
                 $(this).addClass('on');
                 selectList.slideDown(duration);
-    
+
                 selectItem.on('click', function() {
                     let chooseItem = $(this).data('value');
-    
+
                     $('select').val(chooseItem).attr('selected', 'selected');
-                    selectHead.text( $(this).find('span').text() );
-    
+                    selectHead.text($(this).find('span').text());
+
                     selectList.slideUp(duration);
                     selectHead.removeClass('on');
                 });
-    
+
             } else {
                 $(this).removeClass('on');
                 selectList.slideUp(duration);
             }
         });
     });
+
 });
--- gulpFile.js
+++ gulpFile.js
@@ -28,6 +28,7 @@ gulp.task("style", function() {
             "node_modules/slick-carousel/slick/slick.css",
             "node_modules/magnific-popup/dist/magnific-popup.css",
             "node_modules/rateyo/src/jquery.rateyo.css",
+            "node_modules/ion-rangeslider/css/ion.rangeSlider.css",
         ])
         .pipe(concat("libs.min.css"))
         .pipe(cssmin())
@@ -41,6 +42,7 @@ gulp.task("script", function() {
             "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
             "node_modules/mixitup/dist/mixitup.js",
             "node_modules/rateyo/src/jquery.rateyo.js",
+            "node_modules/ion-rangeslider/js/ion.rangeSlider.js",
         ])
         .pipe(concat("libs.min.js"))
         .pipe(uglify())
