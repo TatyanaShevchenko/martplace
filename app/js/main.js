f1ddf747d15445904eca6326ba79d36dbabd1658
Tatyana Shevchenko <dafna2011@gmail.com>
2020-06-09 14:30:55 +0300

    complete product content

--- app/js/main.js
+++ app/js/main.js
@@ -110,5 +110,14 @@ $(function() {
         $(".header__box").toggleClass('active');
     });
 
+    $('.product__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+            $('.product__tabs').find('.tab-item').removeClass('active-tab').hide();
+            $('.product__tabs .tabs').find('.tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+        });
+
  var mixer = mixitup('.new-items__box');
 });
